8e0f891fefa60cb2f340551f859a454d2bd6f468
M Ivan Hanif V <ivanhanif03@gmail.com>
2023-11-04 20:29:56 +0700

    .

--- app/Views/invitation.php
+++ app/Views/invitation.php
@@ -24,27 +24,86 @@
 
     <style>
         h1 {
-            font-size: 200%;
+            font-size: 450%;
+        }
+
+        .bg-cover {
+            background-image: url("img/bg-cover.jpg");
+            height: 100vh;
+        }
+
+        .carousel-inner .carousel-item {
+            height: 500px;
+            background-size: cover;
+            background-position: center center;
         }
     </style>
 </head>
 
 <body>
-    <div class="container-fluid">
+    <div class="bg-cover" id="cover" style="display: none;">
         <div class="d-flex align-items-center justify-content-center vh-100">
             <div class="row text-center">
                 <div class="col-12">
-                    <h4>The Wedding</h4>
+                    <h2>The Wedding</h2>
                 </div>
                 <div class="col-12" style="font-family:Vidaloka;">
                     <h1>Irti & Ivan</h1>
                 </div>
                 <div class="col-12">
-                    <h5>30 April 2024</h5>
+                    <h4>30 April 2024</h4>
+                </div>
+                <div class="col-12 mt-5">
+                    Kepada Yth. <br>
+                    <b>Ivan Hanif</b>
+                </div>
+                <div class="col-12 mt-4">
+                    <button class="btn btn-primary btn-lg" onclick="hideCover()">
+                        Open Invitation
+                    </button>
                 </div>
             </div>
         </div>
     </div>
+
+    <div id="main" style="display: block;">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/bg-cover.jpg" class="d-block w-100" alt="Foto 1">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Foto 1</h5>
+                        <p>Deskripsi 1</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://mdbootstrap.com/img/new/fluid/city/018.jpg" class="d-block w-100" alt="Foto 2">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Foto 2</h5>
+                        <p>Deskripsi 2</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="https://mdbootstrap.com/img/new/fluid/city/018.jpg" class="d-block w-100" alt="Foto 3">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Foto 3</h5>
+                        <p>Deskripsi 3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-12 text-center">
+                    <b>
+                        Assalamualaikum Warahmatullah wabarakatu
+                    </b>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora porro eligendi quod vitae laboriosam eum ipsa harum voluptate numquam recusandae.
+                    </p>
+                </div>
+            </div>
+        </div>
 </body>
 
 <!-- Footer Start -->
@@ -78,5 +137,20 @@
 <script src="<?= base_url('adminkit/js/datatables.js') ?>"></script>
 <script src="<?= base_url('adminkit/js/app.js') ?>"></script>
 <!-- Java Script End -->
+
+<!-- Custom JS -->
+<script>
+    function hideCover() {
+        var cover = document.getElementById("cover");
+        var main = document.getElementById("main");
+        if (cover.style.display === "block") {
+            cover.style.display = "none";
+            main.style.display = "block";
+        } else {
+            cover.style.display = "block";
+            main.style.display = "none";
+        }
+    }
+</script>
 
 </html>
